385b3dc18820be02dc15c8b04dde7681f21cbf11
Alexey Puzankov <itpuza@yandex.ru>
2023-10-28 17:25:52 +0300

    add base test

---
+++ src/test/java/DemoqaTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class DemoqaTest {
+
+        @Test
+        void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+    }
+
