5bd0bc7ac84500255faeaab84675730e9410d7bf
Alexey Puzankov <itpuza@yandex.ru>
2023-10-29 17:01:39 +0300

    add  test forms

--- src/test/java/DemoqaTest.java
+++ src/test/java/DemoqaTest.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,11 +9,50 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class DemoqaTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+
+    }
         @Test
         void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+            open("/automation-practice-form");
+            $("[id=firstName]").setValue("Pasha");
+            $("[id=lastName]").setValue("Technique");
+            $("[id=userEmail]").setValue("Technique@mail.com");
+            $("[for='gender-radio-1']").click();
+            $("[id=userNumber").setValue("1111111111");
+            $("[id=dateOfBirthInput").click();
+            $(".react-datepicker__year-select").selectOption("1993");
+            $(".react-datepicker__month-select").selectOption("January");
+            $(".react-datepicker__day--023:not(.react-datepicker__day--outside-month)").click();
+            $("[id=subjectsInput]").val("Economics").pressEnter();;
+            $("[for='hobbies-checkbox-1']").click();
+            $("#uploadPicture").uploadFromClasspath("123.jpeg");
+            $("[id=currentAddress]").setValue("Moscow");
+            $("#react-select-3-input").val("Haryana").pressEnter();
+            $("#react-select-4-input").val("Panipat").pressEnter();
+            $("#submit").pressEnter();
+
+
+            $(".table-responsive").shouldHave(
+                    text("Pasha Technique"),
+                    text("Technique@mail.com"),
+                    text("Male"),
+                    text("1111111111"),
+                    text("23 January,1993"),
+                    text("Economics"),
+                    text("Sports"),
+                    text("123.jpeg"),
+                    text("Moscow"),
+                    text("Haryana Panipat"));
+
+
+
+        }
     }
-    }
+
 
