dd4d15491334d6a1e3eac76b82f5a7a6a70756c0
Alexey Puzankov <itpuza@yandex.ru>
2023-11-05 13:32:59 +0300

    add  test Selenide #2

---
+++ src/test/java/GitHubSolutions.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class GitHubSolutions {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+
+    void searchSolutions() {
+
+        open("https://github.com/");
+        $("li").sibling(0)
+                .hover();
+        $("a[href='/enterprise']").click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+    }
+
+}
