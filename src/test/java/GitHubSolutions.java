873e40a32e7679f0f93d6041456c2227702e96cc
Alexey Puzankov <itpuza@yandex.ru>
2023-11-05 13:59:03 +0300

    add  test Selenide #2

--- src/test/java/GitHubSolutions.java
+++ src/test/java/GitHubSolutions.java
@@ -3,22 +3,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 public class GitHubSolutions {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
 
     void searchSolutions() {
-
-        open(baseUrl);
+        open("https://github.com/");
         $("li").sibling(0)
                 .hover();
         $("a[href='/enterprise']").click();
